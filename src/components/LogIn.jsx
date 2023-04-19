643b17f180ca8f2d726090f94374d0879c3ece55
Andrew <andrewboug@hotmail.com>
2023-04-19 11:14:56 -0500

    added baseurl to post, and login stuff

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -3,8 +3,18 @@ import React from "react";
 export default function LogIn() {
   return (
     <div>
-      <form input="text" placeholder="Username"></form>
-      <button>Submit</button>
+      <h3>Log In</h3>
+      <form>
+        <label>
+          Username:
+          <input name="username" type="text" />
+        </label>
+        <label>
+          Password:
+          <input name="password" type="text" />
+        </label>
+      </form>
+      <button>Log in to Stranger's Things!!!!!!!</button>
     </div>
   );
 }
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+const BaseUrl =
+  "https://strangers-things.herokuapp.com/api/2301-FTB-ET-WEB-AM/posts";
 
 export default function Posts() {
   return <div> The Posts component</div>;
