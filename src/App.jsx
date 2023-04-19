6fa68cd616d04e779d1cbae9b4f9e9bc5707bbe4
Andrew <andrewboug@hotmail.com>
2023-04-19 10:07:43 -0500

    please work

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Posts from "./components/Posts";
+import Profile from "./components/Profile";
+import LogIn from "./components/LogIn";
+import Register from "./components/Register";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NavBar() {
+  return <div>The NavBar component</div>;
+}
