fc12dc99eae6f91c1a6782d896c2f0d58ca8eb4a
Andrew <andrewboug@hotmail.com>
2023-04-24 11:12:35 -0500

    added gray background color, changed some of the profile component, adeed a new post button without functionality

--- src/components/AfterLoginHeader.jsx
+++ src/components/AfterLoginHeader.jsx
@@ -5,9 +5,9 @@ export default function AfterLoginHeader() {
   return (
     <div>
       <h3>Hello User</h3>
-      <Link to="/posts">Posts</Link>
-      <Link to="/profile">Profile</Link>
-      <Link to="/">Logout</Link>
+      <Link to="/posts">Posts </Link>
+      <Link to="/profile">Profile </Link>
+      <Link to="/">Logout </Link>
     </div>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,7 +5,11 @@ export default function Profile() {
   return (
     <div>
       <AfterLoginHeader />
+      <h1>User's Profile</h1>
+      <h3>About:</h3>
       <p>I am really cool</p>
+      <h3>My posts:</h3>
+      <button>Create New Post</button>
     </div>
   );
 }
