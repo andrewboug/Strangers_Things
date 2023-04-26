612a65dc38417fce11e9d11e8dc76fe77077e1fd
SMeyer-8 <sarah.blue.meyer@gmail.com>
2023-04-26 09:51:11 -0500

    made logout button with functionality to remove token from storage

--- src/components/AfterLoginHeader.jsx
+++ src/components/AfterLoginHeader.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import { useNavigate } from "react-router";
 
 export default function AfterLoginHeader() {
   const { token, user, setToken } = useAuth();
+  const navigate = useNavigate();
   return (
     <div>
      {token ? <p>Welcome, {user.username}</p> : null} 
       <Link to="/posts">Posts </Link>
       <Link to="/profile">Profile </Link>
-      <Link to="/">Logout </Link>
+      <button
+          onClick={() => {
+            // clear the token
+            setToken(null);
+            localStorage.removeItem("token");
+            navigate("/");
+          }}
+        >
+          Log Out
+        </button>
     </div>
   );
 }
