7d5bc07e889e799e48efaa9c4540a9ac275c5404
SMeyer-8 <sarah.blue.meyer@gmail.com>
2023-04-26 10:06:16 -0500

    rendered user name in profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AfterLoginHeader from "./AfterLoginHeader";
+import useAuth from "../hooks/useAuth"; 
 
 export default function Profile() {
+  const {token, user, setToken} = useAuth();
   return (
     <div>
       <AfterLoginHeader />
-      <h1>User's Profile</h1>
+      {token ? <h2>{user.username}'s Profile</h2> : null} 
       <h3>About:</h3>
       <p>I am really cool</p>
       <h3>My posts:</h3>
