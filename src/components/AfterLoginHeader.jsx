e1542d513c8fd869fedd5f8fe787538643003861
SMeyer-8 <sarah.blue.meyer@gmail.com>
2023-04-26 09:42:13 -0500

    added username rendering to login heading

--- src/components/AfterLoginHeader.jsx
+++ src/components/AfterLoginHeader.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 export default function AfterLoginHeader() {
+  const { token, user, setToken } = useAuth();
   return (
     <div>
-      <h3>Hello User</h3>
+     {token ? <p>Welcome, {user.username}</p> : null} 
       <Link to="/posts">Posts </Link>
       <Link to="/profile">Profile </Link>
       <Link to="/">Logout </Link>
