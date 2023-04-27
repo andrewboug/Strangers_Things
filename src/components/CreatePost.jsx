3f6fc1ee6eabd2c68650f26a15d96a1bf9318997
Andrew <andrewboug@hotmail.com>
2023-04-27 09:47:34 -0500

    created a form for the create post component

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -3,7 +3,25 @@ import React from "react";
 export default function CreatePost() {
   return (
     <div>
-      <h1>IM HERE</h1>
+      <form>
+        <label>
+          Title
+          <input name="title" type="text" />
+        </label>
+        <label>
+          Description
+          <input name="Description" type="text" />
+        </label>
+        <label>
+          Price
+          <input name="Price" type="text" />
+        </label>
+        <label>
+          Location
+          <input name="Location" type="text" />
+        </label>
+        <button type="button">Create</button>
+      </form>
     </div>
   );
 }
