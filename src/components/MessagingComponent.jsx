5b14f304d23724e8fe554c8a814b04e21459e184
Andrew <andrewboug@hotmail.com>
2023-04-30 16:51:22 -0500

    added class to send button

--- src/components/MessagingComponent.jsx
+++ src/components/MessagingComponent.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { messagePost, fetchPosts } from '../api/post';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { messagePost, fetchPosts } from "../api/post";
+import { useParams } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 
 async function updateMessageState(postId, setMessages, setPost) {
@@ -12,49 +12,50 @@ async function updateMessageState(postId, setMessages, setPost) {
 }
 
 const MessagingComponent = () => {
- const [messages, setMessages] = useState([]);
- const [newMessage, setNewMessage] = useState('');
- const [post, setPost] = useState([]);
- const { postId } = useParams();
- const { token } = useAuth();
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const [post, setPost] = useState([]);
+  const { postId } = useParams();
+  const { token } = useAuth();
 
+  useEffect(() => {
+    // Fetch messages from the API
 
- useEffect(() => {
-   // Fetch messages from the API
-      
-      updateMessageState(postId, setMessages, setPost);
- }, []);
+    updateMessageState(postId, setMessages, setPost);
+  }, []);
 
+  const handleNewMessageChange = (event) => {
+    setNewMessage(event.target.value);
+  };
 
- const handleNewMessageChange = (event) => {
-   setNewMessage(event.target.value);
- };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // Send a new message to the API
+    let response = await messagePost(newMessage, token, postId);
+    setMessages([...messages, response.data.message]);
+  };
 
-
- const handleSubmit = async (event) => {
-   event.preventDefault();
-   // Send a new message to the API
-  let response = await messagePost(newMessage, token, postId);
-  setMessages([...messages, response.data.message])
- };
-
-
- return (
-   <div>
-     {messages.map((message) => (
-       <div key={message._id}>
-         <p>From: {message.fromUser.username || message.fromUser}</p>
-         <p>Message: {message.content}</p>
-       </div>
-     ))}
-     <form onSubmit={handleSubmit}>
-       <input type="text" placeholder="Message" value={newMessage} onChange={handleNewMessageChange} />
-       <button type="submit">Send</button>
-     </form>
-   </div>
- );
+  return (
+    <div>
+      {messages.map((message) => (
+        <div key={message._id}>
+          <p>From: {message.fromUser.username || message.fromUser}</p>
+          <p>Message: {message.content}</p>
+        </div>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Message"
+          value={newMessage}
+          onChange={handleNewMessageChange}
+        />
+        <button className="button" type="submit">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 };
 
-
 export default MessagingComponent;
-
